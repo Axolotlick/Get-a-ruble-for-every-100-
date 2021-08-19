b6b714957244869e70164d9f985fbc8abc0df7cf
Viktoria Kurochkina <gekkon.kun@gmail.com>
2021-08-19 19:22:42 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 0;
+        int refill = 10;
+      int miles;
+
+      if (refill>1000) {
+          miles = refill / 100;
+      } else {
+          miles = 0;
+      }
+
+        int total = account + refill + miles;
+         System.out.println(total);
+
+    }
+}
