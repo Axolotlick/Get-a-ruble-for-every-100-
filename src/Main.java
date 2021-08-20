b3dfaf39b28fff8381beac66a57fa9d72584df65
Viktoria Kurochkina <gekkon.kun@gmail.com>
2021-08-20 19:00:07 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ public class Main {
     public static void main(String[] args) {
         int account = 0;
         int refill = 10;
-      int miles;
+        int miles;
 
-      if (refill>1000) {
-          miles = refill / 100;
-      } else {
-          miles = 0;
-      }
+        if (refill > 1000) {
+            miles = refill / 100;
+        } else {
+            miles = 0;
+        }
 
         int total = account + refill + miles;
-         System.out.println(total);
+        System.out.println(total);
 
     }
 }
